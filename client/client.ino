70f03f68ca31909008fe25cafbaf0db05f86bf3c
Rogério Fernandes <rogeriomfneto@hotmail.com>
2018-10-17 11:02:19 -0300

    adicionei o programa do cliente

---
+++ client/client.ino
@@ -0,0 +1,51 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "Lufa Lufa - 2.4GHz";
+const char* password = "your-password";
+const char* host = "35.198.24.33";
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  WiFiClient client;
+  const int port = 8000;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  while (client.connected()) {
+    while (client.available() != 0) {
+      Serial.print(client.read());
+    }
+  
+    while (Serial.available()) {
+      client.print(Serial.read());
+    }
+  }
+  client.stop();
+  Serial.println();
+  Serial.println("closing connection");
+}
