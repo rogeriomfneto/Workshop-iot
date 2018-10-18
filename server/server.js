afcc4943b6deeddf4fd2f3610fa38febedb10da8
Gabriel Capella <gabriel@capella.pro>
2018-10-18 09:09:33 -0300

    Add server

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
-var net = require('net');
+
+net = require('net');
 
 var clients = [];
 
-var server = net.createServer(function(socket) {
-    // Adiciona ele na lista
-    clients.push(socket);
-    console.log("Novo cliente");
+net.createServer(function (socket) {
 
-    // Quando um clientes desconectar, tiramos ele da lista
-    socket.on('close', function () {
-        clients.splice(clients.indexOf(socket), 1);
+  socket.name = socket.remoteAddress + ":" + socket.remotePort 
+
+  clients.push(socket);
+
+  socket.write("Welcome " + socket.name + "\n");
+  broadcast(socket.name + " joined the chat\n", socket);
+
+  socket.on('data', function (data) {
+    broadcast(socket.name + "> " + data, socket);
+  });
+
+  socket.on('end', function () {
+    clients.splice(clients.indexOf(socket), 1);
+    broadcast(socket.name + " left the chat.\n");
+  });
+  
+  function broadcast(message, sender) {
+    clients.forEach(function (client) {
+      if (client === sender) return;
+      client.write(message);
     });
+    process.stdout.write(message)
+  }
 
-    socket.on('data', function (data) {
-        console.log(data);
-        for (i in clients)
-            clients[i].write(data);
-    })
-
-    socket.on('error', function(err){
-        console.log(err);
-        socket.end();
-    });
-});
-
-server.on('error', function(err){
-    console.log(err);
-});
-
-
-server.listen(8000, function() {
-    console.log("Servidor iot na porta 8000");
-});
+}).listen(8000);
