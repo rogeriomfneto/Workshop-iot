cfa31083d840cf376ed9eda400614900066ac4bd
Gabriel Capella <gabriel@capella.pro>
2018-10-18 10:47:31 -0300

    Change server menssages

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 net = require('net');
 
 var clients = [];
@@ -8,15 +7,13 @@ var server = net.createServer(function (socket) {
   socket.name = socket.remoteAddress + ":" + socket.remotePort;
   var buffer = "";
 
-
   clients.push(socket);
 
-  socket.write("Welcome " + socket.name + "\n");
-  broadcast(socket.name + " joined the chat\n", socket);
+  socket.write("Bem vindo " + socket.name + "\n");
+  broadcast(socket.name + " entrou no chat.\n", socket);
 
   socket.on('data', function (data) {
     buffer = buffer + data;
-
     while (buffer.indexOf('\n') != -1) {
         var str = buffer.substr(0, buffer.indexOf('\n')+1);
         buffer = buffer.substr(buffer.indexOf('\n')+1);
@@ -26,12 +23,11 @@ var server = net.createServer(function (socket) {
 
   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1);
-    broadcast(socket.name + " left the chat.\n");
+    broadcast(socket.name + " saiu do chat.\n");
   });
 
   socket.on('error', function (err) {
     clients.splice(clients.indexOf(socket), 1);
-    console.log(err);
   });
   
   function broadcast(message, sender) {
@@ -40,7 +36,6 @@ var server = net.createServer(function (socket) {
     });
     process.stdout.write(message)
   }
-
 });
 
 server.timeout = 0;
